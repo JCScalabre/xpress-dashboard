090c542e9f1dee509c9664a862d1539f9f036d0d
Eva Simon <eva.simon@gmail.com>
2018-05-31 13:59:07 -0500

    implemented vertival navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard.js';
+import NavBar1 from './components/NavBar1.js';
+
+const topItems = [
+	{ as: "a", content: "Dashboard/Home", key: "dashboard", icon: "dashboard" },
+	{ as: "a", content: "Profile", key: "profile", icon: "user" },
+	{ as: "a", content: "Properties", key: "properties", icon: "home" },	
+	{ as: "a", content: "Referrals", key: "referrals", icon: "location arrow" }
+];
+
+const bottomItems = [
+	{ as: "a", content: "Logout", key: "logout", icon: "sign out" }
+];
 
 export default class App extends Component {
+	
 	render() {
+
 		return (
 			<Router>
-				<Route path="/" component={Dashboard} />
+				<div>
+					<NavBar1 topItems={topItems} bottomItems={bottomItems}>
+						<Route path="/" component={Dashboard} />
+					</NavBar1>
+				</div>
 			</Router>
 		);
 	}
