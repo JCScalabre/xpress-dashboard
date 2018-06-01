557276ea13c68d5eb05328fb6576f62b712d4723
JC Scalabre <jc_scalabre@hotmail.com>
2018-06-01 11:47:02 -0500

    added firebase functionality to Main.js

---
+++ src/utils/FirebaseConfig.js
@@ -0,0 +1,9 @@
+// // Initialize Firebase
+export default {
+	apiKey: 'AIzaSyCDhNdOzqYxJQvF6PcSvNlk-6r5Y0RBTos',
+	authDomain: 'xpress-react.firebaseapp.com',
+	databaseURL: 'https://xpress-react.firebaseio.com',
+	projectId: 'xpress-react',
+	storageBucket: 'xpress-react.appspot.com',
+	messagingSenderId: '104416720495'
+};
